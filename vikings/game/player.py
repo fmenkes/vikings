1670272188e375aaa96c3703106c8ee5f1b30314
fmenkes <francis.menkes@gmail.com>
2015-06-07 21:30:29 +0200

    Game loop and disembarking
    Added a unit that disembarks when the player ship touches land and can
    re-embark later. Added a game loop in order to do it.

--- vikings/game/player.py
+++ vikings/game/player.py
@@ -6,9 +6,10 @@ import random
 
 
 class Player(pyglet.sprite.Sprite):
+    """Class that playable objects (units and ships) inherit from."""
     
     def __init__(self, window, tile_size, game_board, *args, **kwargs):
-        super(Player, self).__init__(img=resources.ship_tile, *args, **kwargs)
+        super(Player, self).__init__(*args, **kwargs)
 
         self.tile_size = tile_size
         self.game_board = game_board
@@ -17,61 +18,59 @@ class Player(pyglet.sprite.Sprite):
         self.max_width = self.window.width - self.tile_size
         self.key_handler = key.KeyStateHandler()
         self.event_handlers = [self, self.key_handler]
-
-        while True:
-            random_tile = game_board[random.randrange(0, len(game_board))]
-            if type(random_tile) is tile.WaterTile:
-                self.x = random_tile.x
-                self.y = random_tile.y
-                break
+        self.new_objects = []
+        self.check_for_removal = False
+        self.can_traverse_water = False
+        self.can_be_transported = False
+        self.saved_position = ()
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == key.H:
+        if symbol == key.H or symbol == key.LEFT:
             if self.x > 0:
                 self.x -= self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.x += self.tile_size
-        elif symbol == key.L:
+        elif symbol == key.L or symbol == key.RIGHT:
             if self.x < self.max_width:
                 self.x += self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.x -= self.tile_size
-        elif symbol == key.J:
+        elif symbol == key.J or symbol == key.DOWN:
             if self.y > 0:
                 self.y -= self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.y += self.tile_size
-        elif symbol == key.K:
+        elif symbol == key.K or symbol == key.UP:
             if self.y < self.max_height:
                 self.y += self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.y -= self.tile_size
         elif symbol == key.Y:
             if self.y < self.max_height and self.x > 0:
                 self.y += self.tile_size
                 self.x -= self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.y -= self.tile_size
                     self.x += self.tile_size
         elif symbol == key.U:
             if self.y < self.max_height and self.x < self.max_width:
                 self.y += self.tile_size
                 self.x += self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.y -= self.tile_size
                     self.x -= self.tile_size
         elif symbol == key.B:
             if self.y > 0 and self.x > 0:
                 self.y -= self.tile_size
                 self.x -= self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.y += self.tile_size
                     self.x += self.tile_size
         elif symbol == key.N:
             if self.y > 0 and self.x < self.max_width:
                 self.y -= self.tile_size
                 self.x += self.tile_size
-                if type(self.look_up_tile(self.x, self.y)) is not tile.WaterTile:
+                if not self.check_position(self.x, self.y):
                     self.y += self.tile_size
                     self.x -= self.tile_size
 
@@ -80,3 +79,38 @@ class Player(pyglet.sprite.Sprite):
         y = y/self.tile_size
 
         return self.game_board[((self.window.height/self.tile_size)*x+y)]
+
+    def check_position(self, x, y):
+        if self.can_traverse_water:
+            if type(self.look_up_tile(x, y)) is not tile.WaterTile:
+                player_unit = PlayerUnit(window=self.window, game_board=self.game_board, tile_size=self.tile_size,
+                                         x=self.x, y=self.y, batch=self.batch)
+                self.new_objects.append(player_unit)
+                return False
+        elif type(self.look_up_tile(x, y)) is tile.WaterTile:
+            if self.can_be_transported:
+                self.saved_position = (x, y)
+                self.check_for_removal = True
+            return False
+
+        return True
+
+class PlayerUnit(Player):
+    def __init__(self, *args, **kwargs):
+        super(PlayerUnit, self).__init__(img=resources.unit_tile, *args, **kwargs)
+
+        self.can_be_transported = True
+
+
+class PlayerShip(Player):
+    def __init__(self, *args, **kwargs):
+        super(PlayerShip, self).__init__(img=resources.ship_tile, *args, **kwargs)
+
+        self.can_traverse_water = True
+
+        while True:
+            random_tile = self.game_board[random.randrange(0, len(self.game_board))]
+            if type(random_tile) is tile.WaterTile:
+                self.x = random_tile.x
+                self.y = random_tile.y
+                break
--- vikings/vikings.py
+++ vikings/vikings.py
@@ -7,11 +7,11 @@ TILE_SIZE = 16
 game_window = pyglet.window.Window(1200, 800)
 main_batch = pyglet.graphics.Batch()
 game_board = board.random_board(window=game_window, tile_size=TILE_SIZE, batch=main_batch)
-player_ship = player.Player(window=game_window, game_board=game_board, tile_size=TILE_SIZE, batch=main_batch)
-game_objects = [game_board, player_ship]
+player_ship = player.PlayerShip(window=game_window, game_board=game_board, tile_size=TILE_SIZE, batch=main_batch)
+game_objects = [player_ship]
 
-for handler in player_ship.event_handlers:
-    game_window.push_handlers(handler)
+for event_handler in player_ship.event_handlers:
+    game_window.push_handlers(event_handler)
 
 
 @game_window.event
@@ -19,5 +19,41 @@ def on_draw():
     game_window.clear()
     main_batch.draw()
 
+
+def update(dt):
+    to_add = []
+    to_remove = []
+    give_control = []
+
+    for obj in game_objects:
+        to_add.extend(obj.new_objects)
+        obj.new_objects = []
+        if obj.check_for_removal:
+            to_remove.append(obj)
+
+    for removal in to_remove:
+        for obj in game_objects:
+            if obj.can_traverse_water:
+                if obj.x == removal.saved_position[0] and obj.y == removal.saved_position[1]:
+                    removal.delete()
+                    game_objects.remove(removal)
+                    give_control.append(obj)
+
+    if to_add:
+        for obj in game_objects:
+            for handler in obj.event_handlers:
+                game_window.remove_handlers(handler)
+        for obj in to_add:
+            for handler in obj.event_handlers:
+                game_window.push_handlers(handler)
+
+    for obj in give_control:
+        for handler in obj.event_handlers:
+            game_window.push_handlers(handler)
+
+    game_objects.extend(to_add)
+
+
 if __name__ == '__main__':
-   pyglet.app.run()
+    pyglet.clock.schedule_interval(update, 1/120.0)
+    pyglet.app.run()
